8ea324ef6e01da2f977a6be3ed7af6c1137f02c6
jmjcando <jayjari319@yahoo.com>
2014-11-16 09:01:23 -0500

    bizlogic stiffed into service, removed from ctrl.  nice ng app now

---
+++ BookmarkCtrl.js
@@ -0,0 +1,11 @@
+/* Controllers */
+angular.module('BookmarkApp')
+    .controller('BookmarkCtrl', function (BookmarkService) {
+        this.folders = BookmarkService.folderData;
+
+        this.openMultiUrls = function (urls) {
+            angular.forEach(urls, function (value) {
+                window.open(value);
+            });
+        };
+    });
